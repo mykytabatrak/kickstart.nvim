91e552563d0c285a6d8422bee6b6fca6b01a30ae
Mykyta Batrak <mykbatrak@gmail.com>
2025-01-03 20:51:47 -0600

    Modify neo-tree config

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,10 +11,26 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '\\', ':Neotree source=last reveal=true<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
+    close_if_last_window = false,
+    open_files_do_not_replace_types = { 'trouble', 'qf' },
     filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+    buffers = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+    git_status = {
       window = {
         mappings = {
           ['\\'] = 'close_window',
